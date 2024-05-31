f432642360082f3006b9bc6a71317f5b5c40b0dd
Mukhamed Khasanza <fragilehm2411@gmail.com>
2024-05-31 09:52:43 +0600

    Compile the app into a single file.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     cssCodeSplit: false,
     rollupOptions: {
       input: {
-        app: './src/main.jsx',
+        app: "./src/main.jsx",
+      },
+      output: {
+        inlineDynamicImports: true,
       },
     },
   },
-  base: 'https://github.com/fragilehm/sendbird-sample',
+  base: "https://github.com/fragilehm/sendbird-sample",
 });
