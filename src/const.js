ce778e6a49706fcd9fd5a00f3a917f1a7b324910
Mukhamed Khasanza <fragilehm2411@gmail.com>
2024-06-14 14:44:48 +0600

    Use sendbird app id.

--- src/const.js
+++ src/const.js
@@ -1,6 +1,7 @@
 // put your own APP_ID here
 // get your app_id -> https://dashboard.sendbird.com/auth/signin
-export const APP_ID = "360FA073-B471-4BFD-BFCE-44FA8060DF23";
+export const APP_ID = "2D7B4CDB-932F-4082-9B09-A1153792DC8D";
+// export const APP_ID = "360FA073-B471-4BFD-BFCE-44FA8060DF23";
 
 export default {
   APP_ID,
