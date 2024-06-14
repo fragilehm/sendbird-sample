04237881bb2be7151b2317c86e16ea3a2bd0bee9
Mukhamed Khasanza <fragilehm2411@gmail.com>
2024-06-14 14:21:16 +0600

    temp build.

--- src/CustomizedApp.jsx
+++ src/CustomizedApp.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 import SBConversation from "@sendbird/uikit-react/GroupChannel";
+import SBChannelList from '@sendbird/uikit-react/GroupChannelList'
 
 export default function CustomizedApp() {
+  const [currentChannelUrl, setCurrentChannelUrl] = useState("");
+
   return (
     <div className="customized-app">
       <div className="sendbird-app__wrap">
+        <SBChannelList
+          selectedChannelUrl={currentChannelUrl}
+          onChannelCreated={(channel) => {
+            setCurrentChannelUrl(channel.url);
+          }}
+          onChannelSelect={(channel) => {
+            setCurrentChannelUrl(channel?.url);
+          }}
+        />
         <div className="sendbird-app__conversation-wrap">
           <SBConversation
-            channelUrl={"sendbird_group_channel_29484954_207b796608cb959bb040ac0658854639c86ac538"}
+            channelUrl={currentChannelUrl}
             onChatHeaderActionClick={() => {
               setShowSettings(true);
             }}
