9c1205f6df1660cb93708fb338fa1a1704cc989e
Mukhamed Khasanza <fragilehm2411@gmail.com>
2024-06-17 11:21:08 +0600

    Use custom app id.

--- src/const.js
+++ src/const.js
@@ -1,9 +1,10 @@
 // put your own APP_ID here
 // get your app_id -> https://dashboard.sendbird.com/auth/signin
-export const APP_ID = '2D7B4CDB-932F-4082-9B09-A1153792DC8D';
+// export const APP_ID = '2D7B4CDB-932F-4082-9B09-A1153792DC8D';
+export const APP_ID = "360FA073-B471-4BFD-BFCE-44FA8060DF23";
 // set your own USER_ID and NICKNAME
-export const USER_ID = 'sendbirdian-200720';
-export const NICKNAME = 'Sendbirdian84';
+export const USER_ID = "2_TESTQA_4c9a5dbc5ea2c8d497102df1086fa831";
+export const NICKNAME = "Sendbirdian84";
 
 export default {
   APP_ID,
