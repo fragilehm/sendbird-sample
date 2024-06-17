787a4dac3e4e78345ebe850f9d94ec5ca65ccd3a
Mukhamed Khasanza <fragilehm2411@gmail.com>
2024-06-17 11:43:58 +0600

    Only render chat window.

--- src/CustomizedApp.jsx
+++ src/CustomizedApp.jsx
@@ -7,12 +7,12 @@ import SBChannelSettings from '@sendbird/uikit-react/ChannelSettings'
 export default function CustomizedApp() {
   // useState
   const [showSettings, setShowSettings] = useState(false)
-  const [currentChannelUrl, setCurrentChannelUrl] = useState('')
+  // const [currentChannelUrl, setCurrentChannelUrl] = useState("")
 
   return (
     <div className="customized-app">
       <div className="sendbird-app__wrap">
-        <div className="sendbird-app__channellist-wrap">
+        {/* <div className="sendbird-app__channellist-wrap">
           <SBChannelList
             selectedChannelUrl={currentChannelUrl}
             onChannelCreated={(channel) => {
@@ -22,17 +22,17 @@ export default function CustomizedApp() {
               setCurrentChannelUrl(channel?.url)
             }}
           />
-        </div>
+        </div> */}
         <div className="sendbird-app__conversation-wrap">
           <SBConversation
-            channelUrl={currentChannelUrl}
+            channelUrl={window?.sendbirdChatVariables?.channelUrl}
             onChatHeaderActionClick={() => {
               setShowSettings(true)
             }}
           />
         </div>
       </div>
-      {showSettings && (
+      {/* {showSettings && (
         <div className="sendbird-app__settingspanel-wrap">
           <SBChannelSettings
             channelUrl={currentChannelUrl}
@@ -41,7 +41,7 @@ export default function CustomizedApp() {
             }}
           />
         </div>
-      )}
+      )} */}
     </div>
   )
 }
