05d810e0b099e23c733bffb456ef7ad31b56888f
Mukhamed Khasanza <mukhamed.khasanza@ultracommerce.co>
2024-07-05 09:38:22 +0600

    Consume sendbird app id from variable.

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,9 @@ import SBProvider from '@sendbird/uikit-react/SendbirdProvider'
 import '@sendbird/uikit-react/dist/index.css'
 import CustomizedApp from './CustomizedApp'
 
-import { APP_ID } from './const'
-
 function App() {
-  if (!APP_ID) {
-    return (
-      <p>Set APP_ID in const.js</p>
-    )
-  }
-
   return (
-    <SBProvider appId={APP_ID} userId={window?.sendbirdChatVariables?.userId}>
+    <SBProvider appId={window?.sendbirdChatVariables?.appId} userId={window?.sendbirdChatVariables?.userId}>
       <CustomizedApp />
     </SBProvider>
   )
